ddac8b9587d708ae10fb803616c8d421f4de1c70
phuocchung123 <chungnvp197@gmail.com>
2024-12-28 11:28:19 +0700

    update from Lab2

--- src/preprocess_utils.py
+++ src/preprocess_utils.py
@@ -59,7 +59,8 @@ def add_mol(mol_dict, mol):
     n_edge = mol.GetNumBonds() * 2
 
     D_list, A_list = _DA(mol)
-    atom_fea1 = np.eye(118, dtype=bool)[[a.GetAtomicNum() for a in mol.GetAtoms()]]
+    # atom_fea1 = np.eye(118, dtype=bool)[[a.GetAtomicNum() for a in mol.GetAtoms()]]
+    atom_fea1 = np.array([a.GetAtomicNum() for a in mol.GetAtoms()]).reshape(-1,1)
     atom_fea2 = np.eye(len(charge_list), dtype=bool)[
         [charge_list.index(a.GetFormalCharge()) for a in mol.GetAtoms()]
     ][:, :-1]
@@ -132,15 +133,16 @@ def add_mol(mol_dict, mol):
     node_attr = np.hstack(
         [
             atom_fea1,
+            atom_fea4,
             atom_fea2,
             atom_fea3,
-            atom_fea4,
             atom_fea5,
             atom_fea6,
             atom_fea7,
             atom_fea8,
             atom_fea9,
-            atom_fea10,
+            atom_fea10
+            
         ]
     )
     mol_dict["n_node"].append(n_node)
@@ -148,23 +150,23 @@ def add_mol(mol_dict, mol):
     mol_dict["node_attr"].append(node_attr)
 
     if n_edge > 0:
-        bond_fea1 = np.eye(len(bond_list), dtype=bool)[
-            [bond_list.index(str(b.GetBondType())) for b in mol.GetBonds()]
-        ]
-        # bond_fea1=[]
-        # for b in mol.GetBonds():
-        #     idx_bond_list = bond_list.index(str(b.GetBondType()))
-        #     if idx_bond_list ==0:
-        #         bond_fea1.append([1,0,0])
-        #     elif idx_bond_list ==1:
-        #         bond_fea1.append([1,1,0])
-        #     elif idx_bond_list ==2:
-        #         bond_fea1.append([1,2,0])
-        #     elif idx_bond_list ==3:
-        #         bond_fea1.append([1,0.5,1])
-        #     elif idx_bond_list ==4:
-        #         bond_fea1.append([0,0,0])
-        # bond_fea1=np.array(bond_fea1).reshape(-1,3)
+        # bond_fea1 = np.eye(len(bond_list), dtype=bool)[
+        #     [bond_list.index(str(b.GetBondType())) for b in mol.GetBonds()]
+        # ]
+        bond_fea1=[]
+        for b in mol.GetBonds():
+            idx_bond_list = bond_list.index(str(b.GetBondType()))
+            if idx_bond_list ==0:
+                bond_fea1.append([1,0,0])
+            elif idx_bond_list ==1:
+                bond_fea1.append([1,1,0])
+            elif idx_bond_list ==2:
+                bond_fea1.append([1,2,0])
+            elif idx_bond_list ==3:
+                bond_fea1.append([1,0.5,1])
+            elif idx_bond_list ==4:
+                bond_fea1.append([0,0,0])
+        bond_fea1=np.array(bond_fea1).reshape(-1,3)
         
         bond_fea2 = np.array([_stereochemistry(b) for b in mol.GetBonds()], dtype=bool)
         bond_fea3 = np.array(
@@ -172,7 +174,7 @@ def add_mol(mol_dict, mol):
             dtype=bool,
         )
 
-        edge_attr = np.hstack([bond_fea1, bond_fea2 ,bond_fea3])
+        edge_attr = np.hstack([bond_fea1, bond_fea2, bond_fea3])
         edge_attr = np.vstack([edge_attr, edge_attr])
         bond_loc = np.array(
             [[b.GetBeginAtomIdx(), b.GetEndAtomIdx()] for b in mol.GetBonds()],
@@ -191,7 +193,7 @@ def add_mol(mol_dict, mol):
 def add_dummy(mol_dict):
     n_node = 1
     n_edge = 0
-    node_attr = np.zeros((1, 153))
+    node_attr = np.zeros((1, 36))
     mol_dict["n_node"].append(n_node)
     mol_dict["n_edge"].append(n_edge)
     mol_dict["node_attr"].append(node_attr)
@@ -204,12 +206,12 @@ def dict_list_to_numpy(mol_dict):
     mol_dict["n_edge"] = np.array(mol_dict["n_edge"]).astype(int)
     mol_dict["node_attr"] = np.vstack(mol_dict["node_attr"])
     if np.sum(mol_dict["n_edge"]) > 0:
-        mol_dict["edge_attr"] = np.vstack(mol_dict["edge_attr"]).astype(bool)
+        mol_dict["edge_attr"] = np.vstack(mol_dict["edge_attr"])
         mol_dict["src"] = np.hstack(mol_dict["src"]).astype(int)
         mol_dict["dst"] = np.hstack(mol_dict["dst"]).astype(int)
     else:
-        mol_dict["edge_attr"] = np.empty((0, len(bond_list) + 4)).astype(bool)
-        # mol_dict["edge_attr"] = np.empty((0, 3+4))
+        # mol_dict["edge_attr"] = np.empty((0, len(bond_list) + 4))
+        mol_dict["edge_attr"] = np.empty((0, 3+4))
         mol_dict["src"] = np.empty(0).astype(int)
         mol_dict["dst"] = np.empty(0).astype(int)
 
