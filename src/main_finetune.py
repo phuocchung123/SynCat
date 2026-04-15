7fc4ce0640fd17b0b3860e67aa023c563ca03790
Medicine-Artificial-Intelligence <lelaihoangson@gmail.com>
2026-04-16 03:27:34 +0700

    update from lab3

--- src/main_finetune.py
+++ src/main_finetune.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
     arg_parser.add_argument("--monitor_folder", type=str, default="../Data/monitor/")
     arg_parser.add_argument("--Data_folder", type=str, default="../Data/")
     arg_parser.add_argument(
-        "--data_csv", type=str, default="raw/clustered_data_with_Ea_splitted_dropna_canonicalized.csv.gz"
+        "--data_csv", type=str, default="raw/clustered_data_with_Ea_splitted_dropna_canonicalized_1.csv.gz"
     )
     arg_parser.add_argument("--model_path", type=str, default="../Data/model/")
     arg_parser.add_argument("--model_name", type=str, default="model_regression.pt")
@@ -32,7 +32,7 @@ if __name__ == "__main__":
     arg_parser.add_argument("--y_column", type=str, default="Ea_fwd")
     arg_parser.add_argument("--train_test_split", type=bool, default=True)
     arg_parser.add_argument("--split_column", type=str, default="split_fwd")
-    arg_parser.add_argument("--reaction_column", type=str, default="rxn")
+    arg_parser.add_argument("--reaction_column", type=str, default="smart")
     arg_parser.add_argument("--seed", type=int, default=42)
     args = arg_parser.parse_args()
 
