cec205894ae81357b2d6bee9a04bc1329285d60e
phuocchung123 <chungnvp197@gmail.com>
2026-04-19 16:36:58 +0700

    update data for forward reaction

--- src/main_finetune.py
+++ src/main_finetune.py
@@ -24,14 +24,14 @@ if __name__ == "__main__":
     arg_parser.add_argument("--monitor_folder", type=str, default="../Data/monitor/")
     arg_parser.add_argument("--Data_folder", type=str, default="../Data/")
     arg_parser.add_argument(
-        "--data_csv", type=str, default="raw/xtb_Hieu_filter_1.csv.gz"
+        "--data_csv", type=str, default="raw/forward_reactions_dataset_rxn_split.csv.gz"
     )
     arg_parser.add_argument("--model_path", type=str, default="../Data/model/")
-    arg_parser.add_argument("--model_name", type=str, default="model_regression_Hieu_filter_fwd_log.pt")
-    arg_parser.add_argument("--npz_folder", type=str, default="npz/npz_Hieu_filter_fwd_log")
-    arg_parser.add_argument("--y_column", type=str, default="y_fwd")
+    arg_parser.add_argument("--model_name", type=str, default="model_regression_Hieu_forward_sep.pt")
+    arg_parser.add_argument("--npz_folder", type=str, default="npz/npz_Hieu_forward_sep")
+    arg_parser.add_argument("--y_column", type=str, default="adj_fwd")
     arg_parser.add_argument("--train_test_split", type=bool, default=True)
-    arg_parser.add_argument("--split_column", type=str, default="split_fwd")
+    arg_parser.add_argument("--split_column", type=str, default="split")
     arg_parser.add_argument("--reaction_column", type=str, default="rxn")
     arg_parser.add_argument("--seed", type=int, default=42)
     args = arg_parser.parse_args()
